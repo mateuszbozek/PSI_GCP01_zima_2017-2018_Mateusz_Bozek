b80ef3b55b7a50c463efacbd98c5319b5efc474a
mateuszbozek <mateuszbozek077@gmail.com>
2017-12-06 12:51:56 +0100

    Scenariusz_2

---
+++ Adaline.java
@@ -0,0 +1,154 @@
+import java.util.Random;
+
+public class Adaline {
+
+    public double[] tablica_wag; //tablica wag
+    public int liczba_liter; //liczba liter
+    public double blad_globalny; //blad globalny
+    public int liczba_wag; //liczba wag
+    public double wspolczynnik_nauki; //wspolczynnik uczenia
+    public double delta;
+
+    public static char[] litery = {'A','B','C','E','G','I','K','M','O','R','T','a','b','c','d','e','f','g','h','l','r'};
+    public static int[] tablica_oczekiwanych_wynikow = {1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0}; //tablica oczekiwanych wynikow
+    public static int[][] tablica_liter_bit = { //tablica danych uczacych
+            {0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1}, //A
+            {1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0}, //B
+            {0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0}, //C
+            {1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0}, //D
+            {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1}, //E
+            {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0}, //F
+            {0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0}, //G
+            {1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1}, //H
+            {0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0}, //I
+            {1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0}, //J
+
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1}, //a
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1}, //b
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1}, //c
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1}, //d
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1}, //e
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0}, //f
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1}, //g
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1}, //h
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1}, //l
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0}, //r
+    };
+
+    public static char[] litery_test = {'K','L','M','N','O','P','R','S','T','U','f','k','m','n','o','p','r','s','u','w'};
+    public static int[][] tablica_liter_bit_test = { //tablica danych testowych
+
+            {1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1}, //K
+            {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1}, //L
+            {1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1}, //M
+            {1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1}, //N
+            {0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0}, //O
+            {1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0}, //P
+            {1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1}, //R
+            {0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0}, //S
+            {1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0}, //T
+            {1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0}, //U
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0}, //f
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1}, //k
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1}, //m
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0}, //n
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0}, //o
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0}, //p
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0}, //r
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0}, //s
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1}, //u
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0}, //w
+    };
+
+    public Adaline() { //konstruktor
+        delta=0;
+        liczba_wag = 35;
+        liczba_liter = 20;
+        wspolczynnik_nauki = 0.05;
+        blad_globalny=0.0;
+        tablica_wag = new double[liczba_wag];
+    }
+
+    public void losowanie_wag(){ //losowanie wag z przedziału <0-1> wraz z ich wyswietlaniem
+        Random randomWeight = new Random();
+        for(int i=0;i<liczba_wag;i++) {
+            tablica_wag[i] = randomWeight.nextDouble();
+        }
+
+        /*for(int i=0;i<liczba_wag;i++)
+            System.out.println("Weights are: w" + i + " = "+ tablica_wag[i]);
+        System.out.println("\n");*/
+    }
+
+    public boolean funkcja_aktywacji(double sum){ // funkcja aktywacji
+        if(sum > 0.5)
+            return true;
+        else
+            return false;
+    }
+
+    public double funkcja_sumujaca(int letter[], double[]weights){ //funkcja sumujaca perceptron
+        double sum = 0.0;
+        for(int i=0; i < liczba_wag; i++)
+            sum += letter[i] * weights[i];
+        return sum;
+    }
+
+    public void testAdaline() {
+        System.out.print("Letters from learning set:  \n");
+        for(int i=0;i<liczba_liter;i++) {
+            System.out.print("Letter "+ litery[i] +" is: ");
+            if(funkcja_aktywacji(funkcja_sumujaca(tablica_liter_bit[i], tablica_wag))) {
+                System.out.println("DUZA LITERA");
+            }else{
+                System.out.println("mala litera");
+            }
+        }
+
+        System.out.print("Letters from testing set:  \n");
+        for(int i=0;i<liczba_liter;i++) {
+            System.out.print("Letter "+ litery_test[i] +" is: ");
+            if(funkcja_aktywacji(funkcja_sumujaca(tablica_liter_bit_test[i], tablica_wag))) {
+                System.out.println("DUZA LITERa ");
+            }else{
+                System.out.println("mala litera ");
+            }
+        }
+
+    }
+
+    public void uczenie() {
+
+        int epoka=0; //liczba epok potrzebnych do nauki
+        boolean granica_bledu = false; //zmienna, stwierdzajaca czy blad jest mozliwy do zaakceptowania
+
+        losowanie_wag(); // losowanie wag
+
+        do{  //podczas jednej epoki nauka z całego zestawu danych uczacych
+
+            epoka++;
+            blad_globalny=0.0; //zerowanie glownego bledu w celu sprawdzenia bledow podczas jednej iteracji
+            for(int i=0;i<liczba_liter;i++) {
+                delta = tablica_oczekiwanych_wynikow[i] - funkcja_sumujaca(tablica_liter_bit[i], tablica_wag);  //obliczanie roznicy pomiedzy wynikiem oczekiwanym a wynikiem otrzymanym
+
+                for(int j=0; j < liczba_wag;j++) // aktualizowanie nowych wag
+                    tablica_wag[j] +=wspolczynnik_nauki*delta*tablica_liter_bit[i][j];
+
+                blad_globalny+=delta*delta;  //aktualizowanie bledu glownego
+            }
+            blad_globalny/=2;
+
+            if(blad_globalny>0.001)
+                { granica_bledu = false; }
+            else
+                { granica_bledu = true; }
+
+        }while(!granica_bledu);
+
+        System.out.println("Liczba epok : " + epoka );
+        System.out.println("Blad globalny : " + blad_globalny);
+        for(int i=0;i<liczba_wag;i++)
+            System.out.println("Waga wejscia dla : wejscie" + (i+1) + " = "+ tablica_wag[i]);
+        System.out.println("\n");
+    }
+}
